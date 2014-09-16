d4cb8db7c4fdef2f1351ba6fafe4cefdaca02798
Mike Barkas <mike@barkas.com>
2014-09-15 23:33:04 -0400

    db class.

---
+++ inc/config.php
@@ -0,0 +1,12 @@
+<?php
+
+// Databse constants.
+defined('DB_SERVER') ? null : define('DB_SERVER', 'localhost');
+defined('DB_USER')   ? null : define('DB_USER', 'gallery');
+defined('DB_PASS')   ? null : define('DB_PASS', 'password');
+defined('DB_NAME')   ? null : define('DB_NAME', 'photo_gallery');
+
+
+
+
+?>
---
+++ inc/database.php
@@ -0,0 +1,39 @@
+<?php
+require_once('config.php');
+
+class MySQLDatabase {
+
+  private $connection;
+
+  function __construct () {
+    $this->open_connection();
+  }
+
+  // Open connection.
+  public function open_connection () {
+    $this->connection = mysql_connect(DB_SERVER, DB_USER, DB_PASS);
+  }
+
+  // Select database.
+  if (!$this->connection) {
+    die('Database connection failed: ' . mysql_error());
+  } else {
+    $db_select = mysql_select_db(DB_NAME, $this->connection);
+    if (!$db_select) {
+      die('Database selection failed: ' . mysql_error());
+    }
+  }
+
+  // Close connection.
+  public function close_connection () {
+    if (isset($this->connection)) {
+      mysql_close($this->connection);
+      unset($this->connection);
+    }
+  }
+}
+
+
+$database = new MySQLDatabase();
+$db =& $database;
+?>
