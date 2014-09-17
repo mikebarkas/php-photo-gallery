1cf0da1fc38caeb5440a502b1a4147dc6e0208b4
Mike Barkas <mike@barkas.com>
2014-09-16 21:52:59 -0400

    Helper functions.

--- inc/functions.php
+++ inc/functions.php
@@ -11,4 +11,30 @@ function __autoload($class_name) {
   }
 }
 
+// Redirect page.
+function redirect_to($location = NULL) {
+  if ($location != NULL) {
+    header("Location: {$location}");
+    exit;
+  }
+}
+
+// Output a message.
+function output_message($message = '') {
+  if (!empty($message)) {
+    return "<p class=\"message\">{$message}</p>";
+  } else {
+    return "";
+  }
+}
+
+// Remove leading zeros from dates.
+function strip_zeros_from_date($marked_string = '') {
+  // Remove marked zeros.
+  $no_zeros = str_replace('*0', '', $marked_string);
+  // Remove remainig marks.
+  $clean_string = str_replace('*', '', $no_zeros);
+  return $clean_string;
+}
+
 ?>
---
+++ public/admin/index.php
@@ -0,0 +1,17 @@
+<?php
+require_once('../../inc/functions.php');
+require_once('../../inc/session.php');
+if (!$session->is_logged_in()) {redirect_to('login.php');}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Photo Gallery</title>
+</head>
+<body>
+
+<h1>Photo Gallery</h1>
+
+
+</body>
+</html>
