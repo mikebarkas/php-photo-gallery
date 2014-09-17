64b7b6a42e1118d349c1793d3ae2f69fbede4373
Mike Barkas <mike@barkas.com>
2014-09-16 21:19:31 -0400

    Autoloader.

--- inc/functions.php
+++ inc/functions.php
@@ -1,5 +1,14 @@
 <?php
 
-
+// Autoload attempt.
+function __autoload($class_name) {
+  $class_name = strtolower($class_name);
+  $path = "../inc/{$class_name}.php";
+  if (file_exists($path)) {
+    require_once($path);
+  } else {
+    die("The file {$class_name}.php could not be found.");
+  }
+}
 
 ?>
--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+require_once('../inc/functions.php');
 require_once('../inc/database.php');
 require_once('../inc/user.php');
 
