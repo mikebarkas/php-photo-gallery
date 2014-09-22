eec08fccd4b2e4a3f4c94a3dbf987b48dc7ce7d0
Mike Barkas <mike@barkas.com>
2014-09-22 18:06:36 -0400

    Site and lib path.

--- inc/functions.php
+++ inc/functions.php
@@ -3,7 +3,7 @@
 // Autoload attempt.
 function __autoload($class_name) {
   $class_name = strtolower($class_name);
-  $path = "../inc/{$class_name}.php";
+  $path = LIB_PATH.DS."{$class_name}.php";
   if (file_exists($path)) {
     require_once($path);
   } else {
--- inc/initialize.php
+++ inc/initialize.php
@@ -6,22 +6,23 @@
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 
 // Site root.
-//defined('SITE_ROOT') ? null : define('SITE_ROOT', '');
+defined('SITE_ROOT') ? null :
+ define('SITE_ROOT', DS.'Users'.DS.'Mike'.DS.'Development'.DS.'php'.DS.'php-photo-gallery');
 
 // Lib path.
-//defined('LIB_PATH') ? null : define('LIB_PATH', SITE_ROOT.DS.'inc');
+defined('LIB_PATH') ? null : define('LIB_PATH', SITE_ROOT.DS.'inc');
 
 // Load config first.
-require_once('config.php');
+require_once(LIB_PATH.DS.'config.php');
 
 // Load basic functions.
-require_once('functions.php');
+require_once(LIB_PATH.DS.'functions.php');
 
 // Load core objects.
-require_once('session.php');
-require_once('database.php');
+require_once(LIB_PATH.DS.'session.php');
+require_once(LIB_PATH.DS.'database.php');
 
-// Load user class.
-require_once('user.php');
-require_once('photograph.php');
+// Load other class.
+require_once(LIB_PATH.DS.'user.php');
+require_once(LIB_PATH.DS.'photograph.php');
 ?>
