202be2fe24ac21c148c8fbdc9af8af11a1c6eceb
Mike Barkas <mike@barkas.com>
2014-09-25 21:42:17 -0400

    public photo display.

--- public/display_photos.php
+++ public/display_photos.php
@@ -9,7 +9,9 @@ $photos = Photograph::find_all();
 <?php foreach ($photos as $photo) : ?>
   
   <div class="photo">
-  <img src="../<?php echo $photo->image_path(); ?>" width="400"/>
+  <a href="photo.php?id=<?php echo $photo->id; ?>" title="<?php echo $photo->caption;?>">
+    <img src="../<?php echo $photo->image_path(); ?>" width="400"/>
+  </a>
   <p class="caption"><?php echo $photo->caption; ?></p>
   </div>
 
---
+++ public/photo.php
@@ -0,0 +1,22 @@
+<?php
+require_once('../inc/initialize.php');
+if (empty($_GET['id'])) {
+  $session->message('No photograph ID was provided.');
+  redirect_to('display_photos.php');
+}
+
+$photo = Photograph::find_by_id($_GET['id']);
+if (!$photo) {
+  $session->message('The photo could not be located.');
+  redirect_to('display_photos.php');
+}
+?>
+
+<?php include_layout_template('header.php'); ?>
+<p><a href="display_photos.php">&laquo; Back</a></p>
+<div>
+  <img src="<?php echo $photo->image_path();?>" width="680" />
+  <p class="caption"><?php echo $photo->caption;?></p>
+</div>
+
+<?php include_layout_template('footer.php'); ?>
