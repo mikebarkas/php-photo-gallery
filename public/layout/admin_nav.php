b0b0283872bee53cde0af6d20bb955be47af6f49
Mike Barkas <mike@barkas.com>
2014-09-25 21:07:19 -0400

    Logout, to be continued.

--- public/layout/admin_nav.php
+++ public/layout/admin_nav.php
@@ -1,5 +1,6 @@
-<ul class="menu">
-<li><a href="list_photos.php">List Photos</a></li>
-<li><a href="photo_upload.php">Photo Upload</a></li>
-<li><a href="logfile.php">Log File</a></li>
-</ul>
+  <ul class="menu">
+    <li><a href="list_photos.php">List Photos</a></li>
+    <li><a href="photo_upload.php">Photo Upload</a></li>
+    <li><a href="logfile.php">Log File</a></li>
+    <li><a href="logout.php">Logout</a></li>
+  </ul>
